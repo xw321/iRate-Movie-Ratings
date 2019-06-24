fa76576445b265a4d4dcf70872bab0d8c8282a28
Xun Wang <vincent.1994.wang@gmail.com>
2019-06-23 22:29:02 -0700

    add movieRating & freeGift function

--- iRate/src/Helper.java
+++ iRate/src/Helper.java
@@ -1,4 +1,4 @@
-import java.sql.Timestamp;
+import java.sql.*;
 
 public class Helper {
 
@@ -27,5 +27,60 @@ public class Helper {
                 + "review_date >= select timestamp({fn TIMESTAMPADD(SQL_TSI_DAY, -3, " + date
                 + ")}) from sysibm.sysdummy1"
                 + "GROUP BY Endorsement.review_id ORDER BY COUNT DESC limit(1)";
+    }
+
+    public static void freeGift(Connection conn, String date) {
+        if (date.matches("[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])")) {
+            // Date format: yyyy-MM-dd
+            String endTime = " 23:59:59";
+            String startTime = " 00:00:00";
+
+            Timestamp startTimeStamp = Timestamp.valueOf(date + startTime);
+            Timestamp endTimeStamp = Timestamp.valueOf(date + endTime);
+
+
+            // execute query
+
+            try {
+                String query0 = "select customer_Name from Customer INNER JOIN Endorsement ON " +
+                        "Customer.customer_id = Endorsement.endorser_id WHERE timestamp(Endorsement.endorse_date) BETWEEN (?) AND (?)";
+                PreparedStatement invoke_freeGift = conn.prepareStatement(query0);
+                invoke_freeGift.setTimestamp(1, startTimeStamp);
+                invoke_freeGift.setTimestamp(2, endTimeStamp);
+
+                ResultSet rs5 = invoke_freeGift.executeQuery();
+                while (rs5.next()) {
+                    //System.out.println("The winner of the free concession items are: ");
+                    System.out.println("Winners of Free Gift on day " + date + ":       " + rs5.getString("customer_Name"));
+
+                }
+                rs5.close();
+            } catch (SQLException ex) {
+                System.out.printf("There is no winner of the free concession items that day");
+            }
+        }
+    }
+
+
+    public static void movieRating(Connection conn, String movie) {
+
+        try {
+            String query1 = "select avg(CAST(rating as FLOAT )) as rat from Review INNER JOIN Movie ON Review.movie_id = Movie.movie_id WHERE Movie.movie_title = (?)";
+            PreparedStatement invoke_avgRating = conn.prepareStatement(query1);
+            invoke_avgRating.setString(1, movie);
+            ResultSet rs5 = invoke_avgRating.executeQuery();
+
+            if (rs5.next()) {
+                if (rs5.getString("rat") != null) {
+                    System.out.println("Rating of movie " + movie + " is: " + rs5.getString("rat"));
+                } else {
+                    System.out.println("No rating for movie " + movie);
+                }
+
+            }
+
+        } catch (SQLException ex) {
+            System.out.printf("There is no winner of the free concession items that day");
+        }
     }
 }
